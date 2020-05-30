db802afb796c482bf3b0c9f4f1de8bcdbe9706c7
Martín Stancanelli <mastanca14@gmail.com>
2020-05-30 12:59:59 -0300

    read files from levels dir and post them

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const inquirer = require('inquirer');
 const loadLevels = require('./load_levels');
 
-require('dotenv').config()
+require('dotenv').config();
 
 inquirer.prompt([
-   {
-       type: 'list',
-       name: 'mode',
-       message: 'Select the mode to run',
-       choices: ['Level loading', 'Quit']
-   } 
-]).then(answers => {
+  {
+    type: 'list',
+    name: 'mode',
+    message: 'Select the mode to run',
+    choices: ['Level loading', 'Quit'],
+  },
+]).then((answers) => {
   switch (answers.mode) {
-      case 'Level loading':
-          loadLevels();
-          
-          break;
-  
-      default:
-          console.log('Bye!');
-          
-          break;
-  }  
-}).catch(error => {
-    if(error.isTtyError) {
-        console.error('Unexpected error, try in another terminal')
-    } else {
-        console.error('Unexpected error')
-    }
-})
+    case 'Level loading':
+      loadLevels();
+
+      break;
+
+    default:
+      console.log('Bye!');
+
+      break;
+  }
+}).catch((error) => {
+  if (error.isTtyError) {
+    console.error('Unexpected error, try in another terminal');
+  } else {
+    console.error('Unexpected error');
+  }
+});
--- load_levels.js
+++ load_levels.js
@@ -1,13 +1,29 @@
 const axios = require('axios').default;
+const path = require('path');
+const fs = require('fs');
 
-module.exports = loadLevels = () => {
-    console.log('load');
-    axios.post(`${process.env.BACKEND_HOST}/levels`, {
-        body: 'body'
-    }).then(response => {
-        
-    }).catch(error => {
-        console.error('Error running level loading');
-        console.error(error);
-    })
+const directoryPath = path.join(__dirname, 'levels');
+
+const loadLevels = () => {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error(`Unable to scan directory: ${err}`);
+    }
+    files.forEach((file) => {
+      fs.readFile(`${directoryPath}/${file}`, (err, data) => {
+        if (err) throw err;
+        const level = JSON.parse(data);
+        axios.post(`${process.env.BACKEND_HOST}/levels`, {
+          body: level,
+        }).then((response) => {
+          console.log(response);
+        }).catch((error) => {
+          console.error('Error running level loading');
+          console.error(error.toJSON());
+        });
+      });
+    });
+  });
 };
+
+module.exports = loadLevels;
